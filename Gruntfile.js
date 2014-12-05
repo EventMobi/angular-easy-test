37fff1dabb79b4a6d03845984045a07298e5e9d9
Rick Eyre <rick.eyre@hotmail.com>
2014-12-05 11:11:59 -0500

    Add grunt-bump for easy package.json bumps.

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,6 +30,21 @@ module.exports = function(grunt) {
         reporters: [ 'progress' ]
       }
     },
+    bump: {
+      options: {
+        files: [ 'package.json' ],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: [ 'package.json' ],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false
+      }
+    }
   });
   
   grunt.registerTask('default', [ 'jshint', 'karma:unit' ]);
